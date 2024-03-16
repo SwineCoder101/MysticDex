44f58d4ae44ac2750531f1d7efc22f7ee5c3095e
Joe Marron <marronjo@tcd.ie>
2024-03-16 09:03:31 +0000

    add encrypted types to dex storage params, constructor

--- foundry/src/MysticDEX.sol
+++ foundry/src/MysticDEX.sol
@@ -1,10 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import {IEncryptedDEX} from "./IEncryptedDEX.sol";
-import { inEuint32, euint32, inEbool } from "../node_modules/@fhenixprotocol/contracts/FHE.sol";
+import { IEncryptedDEX } from "./IEncryptedDEX.sol";
+import { IFHERC20 } from "@fhenixprotocol/contracts/experimental/token/FHERC20/IFHERC20.sol";
+import { inEuint32, euint32, inEbool } from "@fhenixprotocol/contracts/FHE.sol";
 
 contract MysticDEX is IEncryptedDEX {
+    
+    error MysticDex__DepositError();
+    error MysticDex__WithdrawalError();
+    error MysticDex__AmountCannotBeZero();
+    error MysticDex__InsufficientLiquidity();
+    error MysticDex__InsufficientSharePosition();
+
+    IFHERC20 public immutable i_token0;
+    IFHERC20 public immutable i_token1;
+
+    euint32 public s_totalShares;
+    mapping(address => euint32) public s_userLiquidityShares;
+
+    euint32 public s_liquidity0;
+    euint32 public s_liquidity1;
+
+    constructor(address tokenAddress0, address tokenAddress1) {
+        i_token0 = IFHERC20(tokenAddress0);
+        i_token1 = IFHERC20(tokenAddress1);
+    }
+
     function swap(
         inEbool calldata zeroForOne,
         inEuint32 calldata amountIn
