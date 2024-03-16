97d8cf7bfbaf0db5d55d2bba899f9aa4074f465b
Joe Marron <marronjo@tcd.ie>
2024-03-16 01:01:57 +0000

    rename v1 unencrypted dex

--- foundry/src/IDEX.sol
+++ foundry/src/IDEX.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.24;
 
 interface IDEX {
     function swap(bool zeroForOne, uint256 amountIn) external returns (uint256 amountOut);
-    function addLiquidity(uint256 amount0, uint256 amount1) external returns(uint256 poolShares);
+    function addLiquidity(uint256 maxAmount0, uint256 maxAmount1) external returns(uint256 poolShares);
+    function withdrawLiquidity(uint256 poolShares) external returns(uint256 amount0, uint256 amount1);
 }
