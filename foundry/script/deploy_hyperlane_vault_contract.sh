11c9fef7dc321f3d53a9b8afa2eca037b8f21348
SwineCoder101 <liamfaruq@gmail.com>
2024-03-16 20:23:03 +0000

    deploy-hyperlane-vault-with-custom-YBV

--- foundry/script/deploy_hyperlane_vault_contract.sh
+++ foundry/script/deploy_hyperlane_vault_contract.sh
@@ -3,6 +3,6 @@
 # This script deploys the vault contract to the blockchain.
 source .env
 
-forge script ./script/HypERC20CollateralVaultDepositScript.s.sol:HypERC20CollateralVaultDepositScript --chain-id $CHAIN_ID --rpc-url $BASE_SEPOLIA_RPC_URL \
+forge script ./script/HypERC20CollateralVaultDeposit.s.sol:HypERC20CollateralVaultDepositScript --chain-id $CHAIN_ID --rpc-url $BASE_SEPOLIA_RPC_URL \
     --etherscan-api-key $BASESCAN_API_KEY --verifier-url $BASE_SEPOLIA_API_URL \
     --sender $DEPLOYER_ADDRESS --private-key $PRIVATE_KEY --broadcast --verify -vvvv
