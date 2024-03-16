78c6ccc057f8ced572b13c3b0729bdd3227dfa0c
Joe Marron <marronjo@tcd.ie>
2024-03-16 09:36:19 +0000

    add encrypted swap method

--- foundry/src/IEncryptedDEX.sol
+++ foundry/src/IEncryptedDEX.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import { inEuint32, euint32, inEbool } from "@fhenixprotocol/contracts/FHE.sol";
 
 interface IEncryptedDEX {
-    function swap(inEbool calldata zeroForOne, inEuint32 calldata amountIn) external returns (euint32 amountOut);
+    function swap(bool zeroForOne, inEuint32 calldata amountIn, bytes32 userPublicKey) external returns (euint32);
     function addLiquidity(inEuint32 calldata maxAmount0, inEuint32 calldata maxAmount1) external returns(euint32 poolShares);
     function withdrawLiquidity(inEuint32 calldata poolShares) external returns(euint32 amount0, euint32 amount1);
 }
