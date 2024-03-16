0d20623aade6bf2bc43b979894c559f20449dd12
Joe Marron <marronjo@tcd.ie>
2024-03-16 10:36:10 +0000

    add encrypted liquidity method, lots of refactoring

--- foundry/src/IEncryptedDEX.sol
+++ foundry/src/IEncryptedDEX.sol
@@ -5,6 +5,6 @@ import { inEuint32, euint32, inEbool } from "@fhenixprotocol/contracts/FHE.sol";
 
 interface IEncryptedDEX {
     function swap(bool zeroForOne, inEuint32 calldata amountIn, bytes32 userPublicKey) external returns (euint32);
-    function addLiquidity(inEuint32 calldata maxAmount0, inEuint32 calldata maxAmount1) external returns(euint32 poolShares);
+    function addLiquidity(inEuint32 calldata maxInAmount0, inEuint32 calldata maxInAmount1) external returns(euint32 poolShares);
     function withdrawLiquidity(inEuint32 calldata poolShares) external returns(euint32 amount0, euint32 amount1);
 }
--- foundry/src/MysticDEX.sol
+++ foundry/src/MysticDEX.sol
@@ -22,9 +22,19 @@ contract MysticDEX is IEncryptedDEX {
     euint32 public s_liquidity0;
     euint32 public s_liquidity1;
 
+    euint32 private immutable ZERO;
+    euint32 private immutable ONE;
+    euint32 private immutable TWO;
+    euint32 private immutable THREE;
+
     constructor(address tokenAddress0, address tokenAddress1) {
         i_token0 = IFHERC20(tokenAddress0);
         i_token1 = IFHERC20(tokenAddress1);
+
+        ZERO = FHE.asEuint32(0);
+        ONE = FHE.asEuint32(1);
+        TWO = FHE.asEuint32(2);
+        THREE = FHE.asEuint32(3);
     }
 
     //zeroForOne doesn't need to be encrypted,
@@ -71,9 +81,47 @@ contract MysticDEX is IEncryptedDEX {
     }
 
     function addLiquidity(
-        inEuint32 calldata maxAmount0,
-        inEuint32 calldata maxAmount1
-    ) external override returns (euint32 poolShares) {}
+        inEuint32 calldata maxInAmount0,
+        inEuint32 calldata maxInAmount1
+    ) external override returns (euint32 poolShares) {
+
+        euint32 maxAmount0 = FHE.asEuint32(maxInAmount0);
+        euint32 maxAmount1 = FHE.asEuint32(maxInAmount1);
+
+        //ignore for now
+        // if (maxAmount0 == 0 || maxAmount1 == 0) {
+        //     revert MysticDex__AmountCannotBeZero();
+        // }
+
+        euint32 optAmount0;
+        euint32 optAmount1;
+
+        ebool liquidity0Zero = FHE.eq(s_liquidity0, ZERO);
+        ebool liquidity1Zero = FHE.eq(s_liquidity1, ZERO);
+
+        ebool liquidity0or1Zero = FHE.or(liquidity0Zero, liquidity1Zero);
+
+        (euint32 tempOptAmount0, euint32 tempOptAmount1) = calculateCPLiquidityReq(
+            maxAmount0,
+            maxAmount1
+        );
+
+        optAmount0 = FHE.select(liquidity0or1Zero, maxAmount0, tempOptAmount0); 
+        optAmount1 = FHE.select(liquidity0or1Zero, maxAmount1, tempOptAmount1); 
+
+        i_token0.transferFromEncrypted(msg.sender, address(this), optAmount0);
+        i_token1.transferFromEncrypted(msg.sender, address(this), optAmount1);
+
+        ebool totalSharesZero = FHE.eq(s_totalShares, ZERO);
+
+        poolShares = FHE.select(totalSharesZero, _sqrt(optAmount0 * optAmount1), calculateTotalSharesNonZeroLiquidity(optAmount0, optAmount1));
+
+        s_liquidity0 = s_liquidity0 + optAmount0;
+        s_liquidity1 = s_liquidity1 + optAmount1;
+
+        s_totalShares = s_totalShares + poolShares;
+        s_userLiquidityShares[msg.sender] = s_userLiquidityShares[msg.sender] + poolShares;
+    }
 
     function withdrawLiquidity(
         inEuint32 calldata poolShares
@@ -90,6 +138,61 @@ contract MysticDEX is IEncryptedDEX {
         } else {
             s_liquidity0 = s_liquidity0 - buyAmount;
             s_liquidity1 = s_liquidity1 + sellAmount;
+        }
+    }
+
+    function calculateCPLiquidityReq(
+        euint32 maxAmount0,
+        euint32 maxAmount1
+    ) private view returns (euint32 optAmount0, euint32 optAmount1) {
+        euint32 cp0 = maxAmount0 * s_liquidity1;
+        euint32 cp1 = maxAmount1 * s_liquidity0;
+
+        ebool cp0eqcp1 = FHE.eq(cp0, cp1);
+
+        optAmount0 = FHE.select(cp0eqcp1, maxAmount0, _checkCp0LtCp1Amount0(cp0, cp1, maxAmount0));
+        optAmount1 = FHE.select(cp0eqcp1, maxAmount1, _checkCp0LtCp1Amount1(cp0, cp1, maxAmount1));
+    }
+
+    function _checkCp0LtCp1Amount0(euint32 cp0, euint32 cp1, euint32 maxAmount0) private view returns (euint32) {
+        ebool cp0ltcp1 = FHE.lt(cp0, cp1);
+        return FHE.select(cp0ltcp1, maxAmount0, cp1 / s_liquidity1);
+    }
+
+    function _checkCp0LtCp1Amount1(euint32 cp0, euint32 cp1, euint32 maxAmount1) private view returns (euint32) {
+        ebool cp0ltcp1 = FHE.lt(cp0, cp1);
+        return FHE.select(cp0ltcp1, cp0 / s_liquidity0, maxAmount1);
+    }
+
+    function calculateTotalSharesNonZeroLiquidity(euint32 optAmount0, euint32 optAmount1) private view returns (euint32 poolShares){
+        euint32 shares0 = (optAmount0 * s_totalShares) / s_liquidity0;
+        euint32 shares1 = (optAmount1 * s_totalShares) / s_liquidity1;
+
+        //ignore for now
+        // if (shares0 == 0 && shares1 == 0) {
+        //     revert MysticDex__InsufficientLiquidity();
+        // }
+
+        ebool shares0ltShares1 = FHE.lt(shares0, shares1);
+
+        poolShares = FHE.select(shares0ltShares1, shares0, shares1);
+    }
+
+    function _sqrt(euint32 y) private view returns (euint32 z) {
+        ebool ygt3 = FHE.gt(y, THREE);
+        ebool yne0 = FHE.ne(y, ZERO);
+
+        euint32 yDefault = FHE.select(yne0, ONE, ZERO);
+
+        z = FHE.select(ygt3, _calculateSqrt(y), yDefault);
+    }
+
+    function _calculateSqrt(euint32 y) private view returns(euint32 z) {
+        z = y;
+        euint32 x = y / TWO + ONE;
+        while (FHE.decrypt(FHE.lt(x,z))) {  //TODO , refactor to avoid decryption
+            z = x;
+            x = (y / x + x) / TWO;
         }
     }
 }
