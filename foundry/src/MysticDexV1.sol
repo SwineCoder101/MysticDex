1bb73d6237bf315a61c4609d3dc144f2e522ea61
Joe Marron <marronjo@tcd.ie>
2024-03-16 01:01:57 +0000

    rename v1 unencrypted dex

--- foundry/src/MysticDexV1.sol
+++ foundry/src/MysticDexV1.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.24;
 import {IDEX} from "./IDEX.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MysticDex is IDEX {
-
+contract MysticDexV1 is IDEX {
     error MysticDex__DepositError();
     error MysticDex__WithdrawalError();
     error MysticDex__AmountCannotBeZero();
     error MysticDex__InsufficientLiquidity();
+    error MysticDex__InsufficientSharePosition();
 
     IERC20 public immutable i_token0;
     IERC20 public immutable i_token1;
@@ -20,7 +20,7 @@ contract MysticDex is IDEX {
     uint256 public s_liquidity0;
     uint256 public s_liquidity1;
 
-    constructor(address tokenAddress0, address tokenAddress1){
+    constructor(address tokenAddress0, address tokenAddress1) {
         i_token0 = IERC20(tokenAddress0);
         i_token1 = IERC20(tokenAddress1);
     }
@@ -29,14 +29,25 @@ contract MysticDex is IDEX {
         bool zeroForOne,
         uint256 sellAmount
     ) external override returns (uint256 amountOut) {
-        if(sellAmount == 0) {
+        if (sellAmount == 0) {
             revert MysticDex__AmountCannotBeZero();
         }
 
-        (IERC20 sellToken, IERC20 buyToken, uint256 sellReserve, uint256 buyReserve) = zeroForOne ? (i_token0, i_token1, s_liquidity0, s_liquidity1) : (i_token1, i_token0, s_liquidity1, s_liquidity0);
+        (
+            IERC20 sellToken,
+            IERC20 buyToken,
+            uint256 sellReserve,
+            uint256 buyReserve
+        ) = zeroForOne
+                ? (i_token0, i_token1, s_liquidity0, s_liquidity1)
+                : (i_token1, i_token0, s_liquidity1, s_liquidity0);
 
-        bool deposit = sellToken.transferFrom(msg.sender, address(this), sellAmount);
-        if(!deposit){
+        bool deposit = sellToken.transferFrom(
+            msg.sender,
+            address(this),
+            sellAmount
+        );
+        if (!deposit) {
             revert MysticDex__DepositError();
         }
 
@@ -47,25 +58,30 @@ contract MysticDex is IDEX {
         settleLiquidity(zeroForOne, sellAmount, amountOut);
 
         bool withdrawal = buyToken.transfer(msg.sender, amountOut);
-        if(!withdrawal){
+        if (!withdrawal) {
             revert MysticDex__WithdrawalError();
         }
     }
 
-    function addLiquidity(uint256 maxAmount0, uint256 maxAmount1) external override returns(uint256 poolShares) {
-        if(maxAmount0 == 0 || maxAmount1 == 0) {
+    function addLiquidity(
+        uint256 maxAmount0,
+        uint256 maxAmount1
+    ) external override returns (uint256 poolShares) {
+        if (maxAmount0 == 0 || maxAmount1 == 0) {
             revert MysticDex__AmountCannotBeZero();
         }
 
-        uint256 optAmount0; 
+        uint256 optAmount0;
         uint256 optAmount1;
 
-        if(s_liquidity0 == 0 || s_liquidity1 == 0){
+        if (s_liquidity0 == 0 || s_liquidity1 == 0) {
             optAmount0 = maxAmount0;
             optAmount1 = maxAmount1;
-        }
-        else{
-            (optAmount0, optAmount1) = calculateCPLiquidityReq(maxAmount0, maxAmount1);
+        } else {
+            (optAmount0, optAmount1) = calculateCPLiquidityReq(
+                maxAmount0,
+                maxAmount1
+            );
         }
 
         i_token0.transferFrom(msg.sender, address(this), optAmount0);
@@ -76,14 +92,14 @@ contract MysticDex is IDEX {
         } else {
             uint256 shares0 = (optAmount0 * s_totalShares) / s_liquidity0;
             uint256 shares1 = (optAmount1 * s_totalShares) / s_liquidity1;
-            
-            if(shares0 == 0 && shares1 == 0){
+
+            if (shares0 == 0 && shares1 == 0) {
                 revert MysticDex__InsufficientLiquidity();
             }
-            
-            if(shares0 < shares1){
+
+            if (shares0 < shares1) {
                 poolShares = shares0;
-            }else{
+            } else {
                 poolShares = shares1;
             }
         }
@@ -95,27 +111,58 @@ contract MysticDex is IDEX {
         s_userLiquidityShares[msg.sender] += poolShares;
     }
 
-    function settleLiquidity(bool zeroForOne, uint256 sellAmount, uint256 buyAmount) private {
-        if(zeroForOne){
+    function withdrawLiquidity(
+        uint256 poolShares
+    ) external override returns (uint256 amount0, uint256 amount1) {
+        if(s_userLiquidityShares[msg.sender] < poolShares) {
+            revert MysticDex__InsufficientLiquidity();
+        }
+
+        amount0 = (poolShares * s_liquidity0) / s_totalShares;
+        amount1 = (poolShares * s_liquidity1) / s_totalShares;
+        
+        if(amount0 == 0 || amount1 == 0) {
+            revert MysticDex__InsufficientSharePosition();
+        }
+
+        s_userLiquidityShares[msg.sender] -= poolShares;
+        s_totalShares -= poolShares;
+
+        s_liquidity0 -= amount0;
+        s_liquidity1 -= amount1;
+
+        i_token0.transfer(msg.sender, amount0);
+        i_token1.transfer(msg.sender, amount1);
+    }
+
+    function settleLiquidity(
+        bool zeroForOne,
+        uint256 sellAmount,
+        uint256 buyAmount
+    ) private {
+        if (zeroForOne) {
             s_liquidity0 += sellAmount;
             s_liquidity1 -= buyAmount;
-        }else{
+        } else {
             s_liquidity0 -= buyAmount;
             s_liquidity1 += sellAmount;
         }
     }
 
-    function calculateCPLiquidityReq(uint256 maxAmount0, uint256 maxAmount1) private view returns (uint256 optAmount0, uint256 optAmount1) {
+    function calculateCPLiquidityReq(
+        uint256 maxAmount0,
+        uint256 maxAmount1
+    ) private view returns (uint256 optAmount0, uint256 optAmount1) {
         uint256 cp0 = maxAmount0 * s_liquidity1;
         uint256 cp1 = maxAmount1 * s_liquidity0;
 
-        if(cp0 == cp1){
+        if (cp0 == cp1) {
             optAmount0 = maxAmount0;
             optAmount1 = maxAmount1;
-        }else if(cp0 < cp1){
+        } else if (cp0 < cp1) {
             optAmount0 = maxAmount0;
             optAmount1 = cp0 / s_liquidity0;
-        }else{
+        } else {
             optAmount1 = maxAmount1;
             optAmount0 = cp1 / s_liquidity1;
         }
--- foundry/test/MysticDexV1.t.sol
+++ foundry/test/MysticDexV1.t.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
-import {MysticDex} from "../src/MysticDex.sol";
+import {MysticDexV1} from "../src/MysticDexV1.sol";
 import {MockERC20} from "./mock/MockERC20.sol";
 
-contract MysticDexTest is Test {
+contract MysticDexV1Test is Test {
 
-    MysticDex public amm;
+    MysticDexV1 public amm;
     MockERC20 public token0;
     MockERC20 public token1;
 
@@ -15,7 +15,7 @@ contract MysticDexTest is Test {
         token0 = new MockERC20("Token0", "T0");
         token1 = new MockERC20("Token1", "T1");
 
-        amm = new MysticDex(address(token0), address(token1));
+        amm = new MysticDexV1(address(token0), address(token1));
 
         token0.approve(address(amm), 1_000_000);
         token1.approve(address(amm), 1_000_000);
@@ -100,32 +100,71 @@ contract MysticDexTest is Test {
         uint256 startingLiq0 = amm.s_liquidity0();
         uint256 startingLiq1 = amm.s_liquidity1();
 
-        uint256 maxAmount0 = 100_000;
-        uint256 maxAmount1 = 100_000;
+        uint256 maxAmount = 100_000;
 
-        token0.approve(address(amm), 100_000);
-        token1.approve(address(amm), 100_000);
+        token0.approve(address(amm), maxAmount);
+        token1.approve(address(amm), maxAmount);
 
-        amm.addLiquidity(maxAmount0, maxAmount1);
+        amm.addLiquidity(maxAmount, maxAmount);
 
-        assertEq(amm.s_liquidity0(), startingLiq0 + maxAmount0);
+        assertEq(amm.s_liquidity0(), startingLiq0 + maxAmount);
         assertEq(amm.s_liquidity0(), token0.balanceOf(address(amm)));
 
-        assertEq(amm.s_liquidity1(), startingLiq1 + maxAmount1);
+        assertEq(amm.s_liquidity1(), startingLiq1 + maxAmount);
         assertEq(amm.s_liquidity1(), token1.balanceOf(address(amm)));
 
         uint256 interimLiquidity0 = amm.s_liquidity0();
         uint256 interimLiquidity1 = amm.s_liquidity1();
 
-        token0.approve(address(amm), 100_000);
-        token1.approve(address(amm), 100_000);
+        token0.approve(address(amm), maxAmount);
+        token1.approve(address(amm), maxAmount);
 
-        amm.addLiquidity(maxAmount0, maxAmount1);
+        amm.addLiquidity(maxAmount, maxAmount);
 
-        assertEq(amm.s_liquidity0(), interimLiquidity0 + maxAmount0);
+        assertEq(amm.s_liquidity0(), interimLiquidity0 + maxAmount);
         assertEq(amm.s_liquidity0(), token0.balanceOf(address(amm)));
 
-        assertEq(amm.s_liquidity1(), interimLiquidity1 + maxAmount1);
+        assertEq(amm.s_liquidity1(), interimLiquidity1 + maxAmount);
         assertEq(amm.s_liquidity1(), token1.balanceOf(address(amm)));
+    }
+
+    function testWithdrawLiquidity() public {
+        uint256 startingLiq0 = amm.s_liquidity0();
+        uint256 startingLiq1 = amm.s_liquidity1();
+
+        uint256 maxAmount = 100_000;
+
+        token0.approve(address(amm), maxAmount);
+        token1.approve(address(amm), maxAmount);
+
+        uint256 shares = amm.addLiquidity(maxAmount, maxAmount);
+
+        assertEq(amm.s_liquidity0(), startingLiq0 + maxAmount);
+        assertEq(amm.s_liquidity0(), token0.balanceOf(address(amm)));
+
+        assertEq(amm.s_liquidity1(), startingLiq1 + maxAmount);
+        assertEq(amm.s_liquidity1(), token1.balanceOf(address(amm)));
+
+        amm.withdrawLiquidity(shares);
+    }
+
+    function testWithdrawTooMuchLiquidity() public {
+        uint256 startingLiq0 = amm.s_liquidity0();
+        uint256 startingLiq1 = amm.s_liquidity1();
+
+        uint256 maxAmount = 100_000;
+
+        token0.approve(address(amm), maxAmount);
+        token1.approve(address(amm), maxAmount);
+
+        uint256 shares = amm.addLiquidity(maxAmount, maxAmount);
+
+        assertEq(amm.s_liquidity0(), startingLiq0 + maxAmount);
+        assertEq(amm.s_liquidity0(), token0.balanceOf(address(amm)));
+
+        assertEq(amm.s_liquidity1(), startingLiq1 + maxAmount);
+        assertEq(amm.s_liquidity1(), token1.balanceOf(address(amm)));
+
+        amm.withdrawLiquidity(shares+1);
     }
 }
